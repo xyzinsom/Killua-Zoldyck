b0e71f43cb579ce9cefd6e870a335953b287cdf3
xyzinsom <132595211+xyzinsom@users.noreply.github.com>
2023-05-07 01:35:38 +0000

    Jawab Salam

--- commands/main/owner.js
+++ commands/main/owner.js
@@ -5,7 +5,22 @@ module.exports = {
     type: "main",
     example: "%prefix%command",
     start: async (killua, m) => {
-        killua.sendText(m.from, "Klik Nomor Di Bawah Ini\n\nwa.me/" + config.owner[0], m)
+        let dlan = `Assalamualaikum Semuanya...
+Ini nomor ownerku yaa....
+wa.me/6281311268263
+        `
+        let buttons = [
+            {buttonId: `verify`, buttonText: {displayText: 'Verify'}, type: 1},
+            {buttonId: `profile`, buttonText: {displayText: 'Profile'}, type: 1}
+        ]
+        let buttonMessage = {
+            image: { url: 'https://telegra.ph/file/3cbcd55496fe07374d158.jpg' },
+            caption: dlan,
+            footer: config.footer,
+            buttons: buttons,
+            headerType: 4
+        }
+        killua.sendMessage(m.from, buttonMessage, { quoted: m })
     },
     noLimit: true,
 }
